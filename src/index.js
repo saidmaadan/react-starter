5d68429788f3a128a67b5d861bbff8eafa5892c7
Said Maadan <me@saidmaadan.com>
2018-02-20 07:24:08 -0600

    Created search bar functionality

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+//const API_KEY = '';
+// Create a new component. This component should produce some HTML
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+
+
+
+// class HelloWorld extends React.component{
+//   render(){
+//     return(<div>Hello World</div>)
+//   }
+// }
