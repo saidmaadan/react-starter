26a34673e24e6c2f2c5c49c6da193e3133dc95f1
Said Maadan <me@saidmaadan.com>
2018-02-20 19:28:52 -0600

    Implemented video list and detail components

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-//const API_KEY = '';
+const API_KEY = '';
+
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App  extends Component {
+  constructor(props){
+    super(props);
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      //this.setState({ videos: videos});
+      this.setState({ videos});
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
-
-
-
-
-// class HelloWorld extends React.component{
-//   render(){
-//     return(<div>Hello World</div>)
-//   }
-// }
