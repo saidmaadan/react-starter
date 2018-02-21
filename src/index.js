b2f6578b38c31168ba802c1cc94e71eee3e7c0ef
Said Maadan <me@saidmaadan.com>
2018-02-21 07:40:48 -0600

    Implemented search capabilities

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -16,8 +17,11 @@ class App  extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch('Ruby on Rails');
+  }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       //this.setState({ videos: videos});
       this.setState({
         videos: videos,
@@ -26,9 +30,10 @@ class App  extends Component {
     });
   }
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={videoSearch}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
